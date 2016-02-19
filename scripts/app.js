322366623631dfcda4617f97bbc410c54a844781
Patrick Ly-Vo <patricklyvo@gmail.com>
2016-02-19 11:31:58 -0800

    Added links to posts and upvotes.

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,7 @@ var app = angular.module('meanNews', []);
 
 app.controller('MainController', ['$scope', function($scope) {
 	$scope.test = 'Hello World!';
+
 	$scope.posts = [
 		{title: 'post 1', upvotes: 5},
 		{title: 'post 2', upvotes: 2},
@@ -9,9 +10,20 @@ app.controller('MainController', ['$scope', function($scope) {
 		{title: 'post 4', upvotes: 9},
 		{title: 'post 5', upvotes: 4}
 	];
+
 	$scope.addPost = function() {
 		if (!$scope.title || $scope.title === '') {return; }
-		$scope.posts.push({title: $scope.title, upvotes: 0});
+		$scope.posts.push({
+			title: $scope.title,
+			link: $scope.link,
+			upvotes: 0
+		});
 		$scope.title = '';
+		$scope.link = '';
 	};
+
+	$scope.incrementUpvotes = function(post) {
+		post.upvotes++;
+	};
+
 }]);
