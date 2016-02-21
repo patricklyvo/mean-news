253b5be2e0bde433e02a79ae84bc9f96c9a34928
Patrick Ly-Vo <patricklyvo@gmail.com>
2016-02-20 16:50:36 -0800

    Functioning backend, added routes.

--- mean-news/app.js
+++ mean-news/app.js
@@ -5,6 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// require mongoose, require mongoose models, and connect
+var mongoose = require('mongoose');
+require('./models/Posts');
+require('./models/Comments');
+
+mongoose.connect('mongodb://localhost/news');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
--- mean-news/routes/index.js
+++ mean-news/routes/index.js
@@ -6,4 +6,101 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// importing mongoose to have handles to Post and Comment models
+var mongoose = require('mongoose');
+// load Post and Comment models
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+
+// --- routes ---
+//GET /posts - return a list of posts and associated metadata
+router.get('/posts', function(req, res, next) {
+	Post.find(function(err, posts) {
+		if (err) { return next(err); }
+
+		res.json(posts);
+	});
+});
+
+// POST /posts - create a new post
+router.post('/posts', function(req, res, next) {
+	var post = new Post(req.body);
+
+	post.save(function(err, post) {
+		if (err) { return next(err); }
+
+		res.json(post);
+	});
+});
+
+// route for preloading post objects
+router.param('post', function(req, res, next, id) {
+	var query = Post.findById(id);
+
+	query.exec(function(err, post) {
+		if (err) { return next(err); }
+		if (!post) { return next(new Error('can\'t find post')); }
+
+		req.post = post;
+		return next();
+	});
+});
+
+// route for preloading comment objects
+router.param('comment', function(req, res, next, id) {
+	var query = Comment.findById(id);
+
+	query.exec(function(err, comment) {
+		if (err) { return next(err); }
+		if (!comment) { return next(new Error('can\'t find comment')); }
+
+		req.comment = comment;
+		return next();
+	});
+});
+
+// GET /posts/:id - return an individual post with associated comments
+router.get('/posts/:post', function(req, res, next) {
+	req.post.populate('comments', function(err, post) {
+		if (err) { return next(err); }
+
+		res.json(req.post);
+	});
+});
+
+// POST /posts/:id/comments - add a new comment to a post by ID
+router.post('/posts/:post/comments', function(req, res, next) {
+	var comment = new Comment(req.body);
+	comment.post = req.post;
+
+	comment.save(function(err, comment) {
+		if (err) { return next(err); }
+
+		req.post.comments.push(comment);
+		req.post.save(function(err, post) {
+			if (err) { return next(err); }
+
+			res.json(comment);
+		});
+	});
+});
+
+// PUT /posts/:id/upvote - upvote a post, using the post ID in the URL
+router.put('/posts/:post/upvote', function(req, res, next) {
+	req.post.upvote(function(err, post) {
+		if (err) { return next(err); }
+
+		res.json(post);
+	});
+});
+
+// PUT /posts/:id/comments/:id/upvote - upvote a comment
+router.put('/posts/:post/comments/:comment/upvote', funtion(req, res, next) {
+	req.post.upvote(function(err, comment) {
+		if (err) { return next(err); }
+
+		res.json(comment);
+	});
+});
+
 module.exports = router;
